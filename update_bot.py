8a8decea6c274865f2c8e74b87e911b881e2cd76
Michael Wekesa <wekesa360@outlook.com>
2023-01-07 22:53:28 +0300

    update bot intents, flows and metadata

---
+++ update_bot.py
@@ -0,0 +1,23 @@
+from sarufi import Sarufi
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+username = os.environ.get('USERNAME')
+password = os.environ.get('PASSWORD')
+
+sarufi = Sarufi(username, password)
+
+def update_sarufi_bot():
+    updated_Nalah_bot = sarufi.update_from_file(
+                            id=331,
+                            intents="data/intents.yaml",
+                            flow="data.,flows.yaml",
+                            metadata="deata,metadata.yaml",
+                        )
+    return updated_Nalah_bot.data
+
+
+if __name__ == "__main__":
+    print(update_sarufi_bot())
+
